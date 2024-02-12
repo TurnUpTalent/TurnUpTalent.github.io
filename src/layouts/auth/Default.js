67e846c0b959fe99ad1d81f13a3274537ef64b03
adambournes21 <adam.bournes2003@gmail.com>
2024-02-12 16:12:22 -0500

    fixed sign In Screen

--- src/layouts/auth/Default.js
+++ src/layouts/auth/Default.js
@@ -71,7 +71,6 @@ function AuthIllustration(props) {
             position='absolute'
             borderBottomLeftRadius={{ lg: "120px", xl: "200px" }}></Flex>
         </Box>
-        <Footer />
       </Flex>
       <FixedPlugin />
     </Flex>
--- src/views/auth/signIn/index.jsx
+++ src/views/auth/signIn/index.jsx
@@ -70,40 +70,37 @@ function SignIn() {
   return (
     <DefaultAuth illustrationBackground={illustration} image={illustration}>
       <Flex
-        maxW={{ base: "100%", md: "max-content" }}
-        w='100%'
-        mx={{ base: "auto", lg: "0px" }}
-        me='auto'
-        h='100%'
-        alignItems='start'
-        justifyContent='center'
-        mb={{ base: "30px", md: "60px" }}
-        px={{ base: "25px", md: "0px" }}
-        mt={{ base: "40px", md: "14vh" }}
-        flexDirection='column'>
-        <Box me='auto'>
-          <Heading color={textColor} fontSize='36px' mb='10px'>
+        direction="column"
+        w="100%"
+        h="100vh"
+        alignItems="start" // Align items to the start of the flex container
+        justifyContent="flex-start"
+        p="25px"
+        pt="30px"
+        overflow="auto"
+      >
+        <Box w="100%" mb="20px">
+          <Heading color={textColor} fontSize="36px" mb="4px"> {/* mb controls space to description */}
             Sign In
           </Heading>
           <Text
-            mb='36px'
-            ms='4px'
             color={textColorSecondary}
-            fontWeight='400'
-            fontSize='md'>
+            fontWeight="400"
+            fontSize="md"
+          >
             Enter your email and password to sign in!
           </Text>
-        </Box>
-        <Flex
-          zIndex='2'
-          direction='column'
-          w={{ base: "100%", md: "420px" }}
-          maxW='100%'
-          background='transparent'
-          borderRadius='15px'
-          mx={{ base: "auto", lg: "unset" }}
-          me='auto'
-          mb={{ base: "20px", md: "auto" }}>
+          </Box>
+          <Flex
+            direction="column"
+            w="420px" // Ensures content width
+            minH="600px" // Minimum height to avoid auto resizing
+            background="transparent"
+            borderRadius="15px"
+            overflow="hidden"
+            me="0" // Ensures content is aligned to the start
+            mx={{ base: "0px", lg: "unset" }} // Adjust based on responsive design needs
+          >
           <Button
             fontSize='sm'
             me='0px'
